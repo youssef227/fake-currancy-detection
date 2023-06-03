2c2ff596ac96393ab215e48d2694b365aa01ec54
Youssef Abbas <abbas.youssef227@gmail.com>
2023-06-03 04:51:40 +0300

    Add files via upload

---
+++ digital/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:digital/tensorflow.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tensorflow Lite',
+      home: Tensorflow(),
+    );
+  }
+}
